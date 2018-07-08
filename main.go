18ff3a550e327f8ae18be8c503b62753f0e7d8fa
Suranart Niamcome <suranartnc@gmail.com>
2018-07-08 23:02:45 +0700

    Add some comments

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
+/*
+	Commands
+	go run 		= compile, then execute => output
+	go build 	= compile => binary file
+	go fmt
+	go install
+	go get
+	go test
+
+	2 Types of packages
+	1. Executable => Run main.main()
+	2. Reusable
+
+	Standard library
+	- fmt
+	- debug
+	- math
+	- crypto
+	- io
+	- encoding
+*/
+
+// Package declaration
 package main
 
+// Import other packages that we need
 import "fmt"
 
 func main() {
