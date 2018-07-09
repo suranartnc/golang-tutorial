254c55fae928fdd889a009d4bf7aba4ee219b9f5
Suranart Niamcome <suranartnc@gmail.com>
2018-07-09 23:47:17 +0700

    Declare a variable

---
+++ cards/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+	Basic Go Types
+	- bool
+	- string
+	- int
+	- float64
+*/
+
+func main() {
+	// Same as
+	// var card string = "Ace of Spades"
+	card := "Ace of Spades"
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
