d19a0af2f6901b9bf037ec35993b55ef7cd1a12e
Suranart Niamcome <suranartnc@gmail.com>
2018-07-11 19:23:05 +0700

    Random in Go

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,7 @@
 package main
 
 func main() {
-	// cards := newDeck()
-	// cards.saveToFile("my_cards")
-
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
