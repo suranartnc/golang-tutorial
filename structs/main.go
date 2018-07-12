1d3b02f7f8e8ea9b1410507db7628b18deb5b8d9
Suranart Niamcome <suranartnc@gmail.com>
2018-07-12 17:24:39 +0700

    Define, declare and update Struct

---
+++ structs/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+
+	var alex person
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+	// alex := person{
+	// 	firstName: "Alex",
+	// 	lastName:  "Anderson",
+	// }
+
+	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
+}
