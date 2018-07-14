c0e3011109d44ec2eb5fe0e17850b6f9408ce7d0
Suranart Niamcome <suranartnc@gmail.com>
2018-07-14 17:30:41 +0700

    Concept of Go Routines

--- channels/main.go
+++ channels/main.go
@@ -19,6 +19,25 @@ func main() {
 	}
 }
 
+/*
+	Go Routines (Threads in Go)
+	- Main routine created when we launched our program
+	- Child routine created by the "go" keyword
+
+	One vs Multiple CPU Core
+	- One CPU Core - Run only one routine at a time until it finishes or it makes a blocking call
+	- Multiple CPU Core - Run one routine on each logical core
+
+	Concurrency
+	- If one thread blocks, another one is picked up
+
+	Parallelism
+	- Use multiple CPU Core to execute multiple threads at the same time
+
+	Create a new thread to run this function
+	go checkLink(link)
+*/
+
 func checkLink(link string) {
 	_, err := http.Get(link)
 	if err != nil {
